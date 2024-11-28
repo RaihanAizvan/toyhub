71763ca8c763be904b282ed34b3c92729507c1b3
RaihanAizvan <aizvanr@gmail.com>
2024-11-28 13:55:33 +0530

    hosting 0.0.3

--- models/admin.models.js
+++ models/admin.models.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const adminUserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    username: { type: String },
     password: { type: String, required: true },
     role: { type: String, required: true },
     email: { type: String, required: true },
